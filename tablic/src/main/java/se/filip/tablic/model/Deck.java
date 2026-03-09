fca1ca07752c757e4f2aa0de8f34b9f742411795
Filip Stanojevic <ficko03@hotmail.com>
2026-03-09 09:03:28 +0100

    Untracked file Deck.java

---
+++ tablic/src/main/java/se/filip/tablic/model/Deck.java
@@ -0,0 +1,16 @@
+package se.filip.tablic.model;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Deck {
+    private List<Card> cards;
+
+    private Deck(){
+        cards = new ArrayList<>();
+    }
+
+    public void Shuffle(){
+
+    }
+}
